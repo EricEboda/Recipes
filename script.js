ae904331b5e7b04f456cf2673e2f9cec70d33e58
CodeEryk <ericjeb3@gmail.com>
2020-06-03 13:36:23 +0100

    Form Updated - Ingredients, Instructions, Assign One Title

---
+++ script.js
@@ -0,0 +1,35 @@
+function addTitle(title) {
+
+  let titleHeading = document.getElementById('titleBox')
+
+  titleHeading.textContent = title.value
+
+  titleHeading.innerHTML
+
+
+  return false;
+}
+
+function addIngredient(ingredient, quantity) {
+  
+  document.getElementById('ingredientBox').textContent='Ingredients:';
+
+  let createUnorderedList = document.createElement('li');
+  document.getElementById('ingredientList').appendChild(createUnorderedList);
+  
+  createUnorderedList.innerHTML += ingredient.value + ": " + quantity.value;
+
+  return false;
+
+}
+
+function addInstruction(instruction) {
+  
+  document.getElementById('instructionBox').textContent='Instructions:';
+
+  let createOrderedList = document.createElement('li');
+  document.getElementById('instructionList').appendChild(createOrderedList);
+  
+  createOrderedList.innerHTML += instruction.value;
+  return false;
+}
