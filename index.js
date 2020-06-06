54bf50b570d8b88eb6e78cdb08ef09eb9af2c5c2
CodeEryk <ericjeb3@gmail.com>
2020-06-06 11:53:07 +0100

    Local Storage Test 1

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const recipes = [];
 
+window.onload = function() {
+  if (JSON.parse(localStorage.getItem("recipes")) != null)
+    recipes = JSON.parse(localStorage.getItem("recipes"));
+  console.log(recipes);
+  display();
+};
+
+function saveRecipe() {
+  
+  if (document.querySelector(".addTitle").value.trim() != "") {
+    recipes.push(document.querySelector(".addTitle").value.trim());
+    if(localStorage.getItem("recipes") == null) {
+      localStorage.setItem("recipes", JSON.stringify(recipes));
+    } else {
+      localStorage.setItem("recipes", JSON.stringify(recipes));
+    }
+    display();
+  }
+}
+
+function display() {
+  document.querySelector(".recipeList").innerHTML = "";
+  for (var i = 0; i < recipes.length; i++)
+    document.querySelector(".recipeList").innerHTML += "<center><div class='element'>" + recipes[i] + "<button class='cookButton btn btn-primary'>Start Cooking</button><img src='images/bin.png' class='dustbin' onclick='del(" + i + ")'></div></center>";
+}
+
+function del(index){
+  recipes.splice(index, 1);
+  if (localStorage.getItem("recipes") == null) {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+  } else {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+  }
+  display();
+}
+
 function getVal() {
   var x = document.getElementById("title.value");
   document.getElementById("titleBox").innerHTML = x.value
@@ -31,30 +67,4 @@ function addInstruction(instruction, time) {
 
   return false;
 
-}
-
-function saveRecipe() {
-  
-  if(document.querySelector(".addTitle").value.trim()!=""){
-    recipes.push(document.querySelector(".addTitle").value.trim());
-    display();
-  }
-  
-}
-
-function display(){
-  document.querySelector(".recipeList").innerHTML = "";
-  for(var i = 0; i < recipes.length ; i++)
-  document.querySelector(".recipeList").innerHTML += "<center><div class='element'>"+recipes[i]+"<button class='cookButton btn btn-primary'>Start Cooking</button><img src='images/bin.png' class='dustbin' onclick='del("+i+")'></div></center>";
-
-}
-
-function del(index){
-  recipes.splice(index, 1);
-  if (localStorage.getItem("recipes") == null) {
-    localStorage.setItem("recipes", JSON.stringify(recipes));
-  } else {
-    localStorage.setItem("recipes", JSON.stringify(recipes));
-  }
-  display();
 }
--- recipes.js
+++
@@ -1,8 +0,0 @@
-const recipes = [];
-  
-  recipes.forEach(function(recipe) {
-    console.log(recipe);
-    $("#recipes").append(`<li class="list-group-item">${recipe.title}</li>`)
-
-
-  })
